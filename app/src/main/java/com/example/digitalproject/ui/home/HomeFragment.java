2a4ff2caca8153ce53c89c5cf3732a72b38e0dc0
Wofyor <dan.shikalov@gmail.com>
2023-05-07 13:38:40 +0300

    home page done

--- app/src/main/java/com/example/digitalproject/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/digitalproject/ui/home/HomeFragment.java
@@ -8,25 +8,34 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.digitalproject.databinding.FragmentHomeBinding;
+import com.example.digitalproject.globalvar.GlobalVariables;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private HomeViewModel homeViewModel;
+    private String token;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
+        homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
-
+        token = ((GlobalVariables) requireActivity().getApplication()).getToken_access();
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        homeViewModel.setToken(token);
+        homeViewModel.name.observe(requireActivity(), s -> binding.textName.setText(s));
+        homeViewModel.surname.observe(requireActivity(), s -> binding.textSurname.setText(s));
+        homeViewModel.phone.observe(requireActivity(), s -> binding.textPhone.setText(s));
+        homeViewModel.email.observe(requireActivity(), s -> binding.textEmail.setText(s));
+        homeViewModel.salary.observe(requireActivity(), s -> binding.textSalary.setText(s));
+        homeViewModel.position.observe(requireActivity(), s -> binding.textPosition.setText(s));
+        homeViewModel.rating.observe(requireActivity(), s -> binding.textRating.setText(s));
+        homeViewModel.getInfo();
+        return binding.getRoot();
     }
 
     @Override
--- app/src/main/java/com/example/digitalproject/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/digitalproject/ui/home/HomeViewModel.java
@@ -1,19 +1,67 @@
 package com.example.digitalproject.ui.home;
 
-import androidx.lifecycle.LiveData;
+import android.annotation.SuppressLint;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class HomeViewModel extends ViewModel {
+    private String token;
+    public MutableLiveData<String> name = new MutableLiveData<>();
+    public MutableLiveData<String> surname = new MutableLiveData<>();
+    public MutableLiveData<String> phone = new MutableLiveData<>();
+    public MutableLiveData<String> email = new MutableLiveData<>();
+    public MutableLiveData<String> salary = new MutableLiveData<>();
+    public MutableLiveData<String> position = new MutableLiveData<>();
+    public MutableLiveData<String> rating = new MutableLiveData<>();
 
-    private final MutableLiveData<String> mText;
-
-    public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+    public void setToken(String token) {
+        this.token = token;
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public HomeViewModel() {
+    }
+
+    public void getInfo() {
+        String url = "http://10.0.2.2:8080/api/persons/get/fullinfo";
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().addHeader("Authorization", "Bearer ".concat(token)).url(url).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+            }
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String resp = Objects.requireNonNull(response.body()).string();
+                    try {
+                        JSONObject jsonObject = new JSONObject(resp);
+                        name.postValue(jsonObject.getString("name"));
+                        surname.postValue(jsonObject.getString("surname"));
+                        phone.postValue(jsonObject.getString("phone"));
+                        email.postValue(jsonObject.getString("email"));
+                        salary.postValue(Long.toString(jsonObject.getLong("salary")));
+                        position.postValue(jsonObject.getString("position"));
+                        rating.postValue(jsonObject.getString("rating"));
+                    } catch (JSONException e) {
+                    }
+                }
+            }
+        });
     }
 }
